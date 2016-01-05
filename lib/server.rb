c49d1175ee4321c76a48667dc2062b7d42e46370
Scott Firestone <scott.firestone.dev@gmail.com>
2016-01-04 17:54:12 -0700

    Have server respond once

--- lib/server.rb
+++ lib/server.rb
@@ -1,3 +1,45 @@
-class Server
+require 'socket'
+require 'pry'
 
+class Server
+  attr_reader :tcp_server
+
+  def initialize(port = 9292)
+    @tcp_server = TCPServer.new(port)
+    @count = 0
+  end
+
+  def send_response(client, args)
+      client.puts args[:output]
+
+    # if var == quit
+    # client.close
+    # else
+    # accept
+    # end
+  end
+
+  def build_response_message(client)
+    response = "<p>Hello, World (0)</p>"
+    output = "<html><head></head><body>#{response}</body></html>"
+    send_response(client, output: output)
+  end
+
+  def build_request_message(client)
+    request_lines = []
+    while line = client.gets and !line.chomp.empty?
+      request_lines << line.chomp
+    end
+    build_response_message(client)
+  end
+
+  def accept
+    build_request_message(tcp_server.accept)
+  end
+
+end
+
+if __FILE__ == $PROGRAM_NAME
+  server = Server.new(9292)
+  server.accept
 end
--- test/server_test.rb
+++ test/server_test.rb
@@ -8,4 +8,10 @@ class ServerTest < Minitest::Test
     assert_instance_of Server, server
   end
 
+  def test_accepts_a_port_as_an_init_argument
+    server = Server.new(9293)
+
+    assert server
+  end
+
 end
