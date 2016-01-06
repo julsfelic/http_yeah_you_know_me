0da55f0f4f972c2825e5ad65daec8bf4b6f21f4a
Julian Feliciano <julsfelic@gmail.com>
2016-01-06 15:36:23 -0700

    move parse request to Request Handler

--- lib/request_handler.rb
+++ lib/request_handler.rb
@@ -1,5 +1,15 @@
 module RequestHandler
 
+  def normalized_lines(request_lines)
+    keys = ["HTTP/1.1", "Host", "Accept:"]
+    formatted_lines = request_lines.select do |line|
+      keys.any? do |key|
+        line.include?(key)
+      end
+    end
+    formatted_lines.map { |line| line.split(" ") }.sort
+  end
+
   def parse_request
     request_lines = []
     while line = client.gets and !line.chomp.empty?
@@ -7,5 +17,4 @@ module RequestHandler
     end
     format_response(normalized_lines(request_lines))
   end
-
 end
--- lib/server.rb
+++ lib/server.rb
@@ -73,17 +73,6 @@ class Server
                "content-length: #{output.length}\r\n\r\n"].join("\r\n")
     send_response(output: output, headers: headers)
   end
-  # RequestHandler
-  def normalized_lines(request_lines)
-    keys = ["HTTP/1.1", "Host", "Accept:"]
-    formatted_lines = request_lines.select do |line|
-      keys.any? do |key|
-        line.include?(key)
-      end
-    end
-    formatted_lines.map { |line| line.split(" ") }.sort
-  end
-
   # Server
   def start_server
     loop do
